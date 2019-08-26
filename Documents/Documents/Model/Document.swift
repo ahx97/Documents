f5c98175412d0a76bdd3b24a90baa2f68ee7c856
Aaron Henry <ahx97@mail.missouri.edu>
2019-08-26 11:57:45 -0500

    create MVC and document struct

---
+++ Documents/Documents/Model/Document.swift
@@ -0,0 +1,18 @@
+//
+//  Document.swift
+//  Documents
+//
+//  Created by Aaron Henry on 8/26/19.
+//  Copyright © 2019 Aaron Henry. All rights reserved.
+//
+
+import Foundation
+
+struct Document {
+    var title: String
+    var description: String
+    var edit: Date
+    
+}
+
+
